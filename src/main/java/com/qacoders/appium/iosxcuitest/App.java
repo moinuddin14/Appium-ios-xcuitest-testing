ea7eea094491dc98d1065c3a0b7178862289a14b
khaja1 <khaja@spotqa.com>
2017-04-09 23:08:13 +0530

    First Commit

---
+++ src/main/java/com/qacoders/appium/iosxcuitest/App.java
@@ -0,0 +1,13 @@
+package com.qacoders.appium.ios-xcuitest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/qacoders/appium/iosxcuitest/IRealDeviceCreditCardTest.java
@@ -0,0 +1,41 @@
+package com.qacoders.appium.iosxcuitest;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by apple on 4/9/17.
+ */
+public class IRealDeviceCreditCardTest {
+
+    static AppiumDriver<MobileElement> driver;
+    static DesiredCapabilities capabilities;
+    public static void main(String[] args) throws MalformedURLException{
+
+                /*"platformName": "iOS",
+                "platformVersion": "10.3.1",
+                "deviceName": "Khaja Moinuddin Mohammed's iPhone",
+                "automationName": "XCuiTest",
+                "app": "/Users/apple/Desktop/CreditCard.app",
+                "udid": "08f02934d61d4216153a36fe78cc445cf6006d0d",
+                "xcodeOrgId": "459N6L7RYM",
+                "xcodeSigningId": "iPhone Developer"*/
+
+        capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "iOS");
+        capabilities.setCapability("platformVersion", "10.3.1");
+        capabilities.setCapability("deviceName", "Khaja Moinuddin Mohammed's iPhone");
+        capabilities.setCapability("app", "/Users/apple/Desktop/CreditCard.app");
+        capabilities.setCapability("udid", "08f02934d61d4216153a36fe78cc445cf6006d0d");
+
+        driver = new IOSDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+        driver.findElementByXPath("//*[1]//*[1]//*[1]//*[2]//*[1]//*[1]//*[2]").sendKeys("1234567890123456");
+        System.out.println(driver.findElementByXPath
+                ("//*[1]//*[1]//*[1]//*[1]//*[1]//*[1]//*[2]").getText());
+    }
+}
